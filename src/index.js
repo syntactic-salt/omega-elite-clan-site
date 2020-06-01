7c0c30e2b0abe12ac0a9981568cc61265107ace3
syntactic-salt <kennethpereira@gmail.com>
2020-06-01 00:02:41 -0400

    rewrite

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const hero = document.getElementById('hero');
+const header = document.getElementById('header');
+
+const handleScroll = () => {
+    const scrollPosition = window.scrollY;
+    hero.style.top = `${-(scrollPosition * 0.3)}px`;
+    header.style.opacity = String(scrollPosition / 400);
+};
+
+handleScroll();
+window.addEventListener('scroll', handleScroll);
+
+document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+    output: {
+        filename: '[name].[contenthash:8].js',
+        path: `${__dirname}/build`,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            hmr: process.env.NODE_ENV === 'development',
+                        },
+                    },
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+        ]
+    },
+    optimization: {
+        minimizer: process.env.NODE_ENV === 'development' ? [] : [new TerserPlugin(), new OptimizeCssAssetsPlugin()],
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html.ejs',
+            filename: './index.html',
+            inject: false,
+            minify: {
+                collapseWhitespace: true,
+                removeComments: true,
+                removeRedundantAttributes: true,
+                removeScriptTypeAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                useShortDoctype: true
+            },
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash:8].css',
+            ignoreOrder: false, // Enable to remove warnings about conflicting order
+        }),
+    ]
+};
